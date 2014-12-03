ed987eec5abbad5f868a7a779a2b41d789e20e94
matteomanzo <imatteo.manzo@gmail.com>
2014-12-03 16:13:47 +0000

    Exercise 1

--- directory.rb
+++ directory.rb
@@ -1,21 +1,19 @@
 def print_header
-  puts "The students of my cohort at Makers Academy" 
-  puts "-------------"
+  print "The students of my cohort at Makers Academy\n-------------\n"
 end
 
 def print_names(students)
   students.each do |student|
-  	puts "#{student[:name]} (#{student[:cohort]} cohort)"
+  	print "#{student[:name].capitalize} (#{student[:cohort].capitalize} cohort)\n"
   end
 end
 
 def print_footer(names)
-puts "Overall we have #{names.length} great students"
+print "Overall we have #{names.length} great students\n"
 end
 
 def input_students
-	puts "Please enter the names of the students"
-	puts "To finish, just hit return twice"
+	print "Please enter the names of the students\nTo finish, just hit return twice\n"
 	# create an empty array
 	students = []
 	# get the name
@@ -24,7 +22,7 @@ def input_students
 	while !name.empty? do
 		# store the student hash into the array
 		students << {:name => name, :cohort => :december}
-		puts "Now we have #{students.length} students"
+		print "Now we have #{students.length} students\n"
 		# get the next name from the user
 		name = gets.chomp
 	end
