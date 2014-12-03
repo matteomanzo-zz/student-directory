2d5953b17c17d6a61f3bf4e71b7335415468da46
matteomanzo <imatteo.manzo@gmail.com>
2014-12-03 16:58:13 +0000

    Exercise 7

--- directory.rb
+++ directory.rb
@@ -1,36 +1,40 @@
+@lineWidth = 50
 def print_header
-  print "The students of my cohort at Makers Academy\n-------------\n"
+  puts ("The students of my cohort at Makers Academy".center(@lineWidth))
+  puts ("-------------".center(@lineWidth))
 end
 
 def print_names(students)
 	i = 0
 	while i < students.length
-  	print "#{i + 1}. #{students[i][:name].capitalize}, #{students[i][:country].capitalize}. Hobbies: #{students[i][:hobbies].capitalize} (#{students[i][:cohort].capitalize} cohort)\n"
-  	i += 1
+		puts ("#{i + 1}. #{students[i][:name].capitalize}, #{students[i][:country].capitalize}. Hobbies: #{students[i][:hobbies].capitalize} (#{students[i][:cohort].capitalize} cohort)".center(@lineWidth))
+		i += 1
     end
 end
 
 def print_footer(names)
-print "Overall we have #{names.length} great students\n"
+	puts ("Overall we have #{names.length} great students".center(@lineWidth))
 end
 
 def input_students
-	print "Please enter the names of the students\nTo finish, just hit return twice\n"
+	puts ("Please enter the names of the students".center(@lineWidth))
+	puts ("To finish, just hit return twice".center(@lineWidth))
 	# create an empty array
 	students = []
 	# get the name
 	name = gets.chomp
 	# while the name is not empty, repeat this code
 	while !name.empty? do
-		puts "Where do you come from?"
+		puts ("Where do you come from?".center(@lineWidth))
 		country = gets.chomp
-		puts "What hobbies do you have?"
+		puts ("What hobbies do you have?".center(@lineWidth))
 		hobbies = gets.chomp
 		# store the student hash into the array
 		students << {:name => name, :cohort => :december, :country => country, :hobbies => hobbies}
-		print "Now we have #{students.length} students\n"
+		puts ("Now we have #{students.length} students".center(@lineWidth))
+		puts ''
 		# get the next name from the user
-		puts "What's your name?"
+		puts ("What's your name?".center(@lineWidth))
 		name = gets.chomp
 	end
 	# return the array of the students
