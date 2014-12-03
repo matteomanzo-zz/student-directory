f419627c492b1099e8a21d6635caecf939e64259
matteomanzo <imatteo.manzo@gmail.com>
2014-12-03 15:53:06 +0000

    Modified array in hashes in array

--- directory.rb
+++ directory.rb
@@ -1,23 +1,23 @@
 
 # students into an array
 students = [
-	"Sanda",
-	"Huy",
-	"Stephi",
-	"David",
-	"Emily",
-	"Kieran",
-	"Richard",
-	"Jacob",
-	"Marcin",
-	"Gabe",
-	"Hannah",
-	"Bibiana",
-	"Chris",
-	"Ptolemy",
-	"Luke",
-	"Danielle",
-	"Josh"
+	{:name => "Sanda", :cohort => :december},
+	{:name => "Huy", :cohort => :december},
+	{:name => "Stephi", :cohort => :december},
+	{:name => "David", :cohort => :december},
+	{:name => "Emily", :cohort => :december},
+	{:name => "Kieran", :cohort => :december},
+	{:name => "Richard", :cohort => :december},
+	{:name => "Jacob", :cohort => :december},
+	{:name => "Marcin", :cohort => :december},
+	{:name => "Gabe", :cohort => :december},
+	{:name => "Hannah", :cohort => :december},
+	{:name => "Bibiana", :cohort => :december},
+	{:name => "Chris", :cohort => :december},
+	{:name => "Ptolemy", :cohort => :december},
+	{:name => "Luke", :cohort => :december},
+	{:name => "Danielle", :cohort => :december},
+	{:name => "Josh", :cohort => :december}
 ]
 
 def print_header
@@ -25,9 +25,9 @@ def print_header
   puts "-------------"
 end
 
-def print_names(names)
-  names.each do |name|
-  	puts name
+def print_names(students)
+  students.each do |student|
+  	puts "#{student[:name]} (#{student[:cohort]} cohort)"
   end
 end
 
